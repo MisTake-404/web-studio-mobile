78be165acf69fbc65dc0dc9a8a40c88b357f8b66
MisTake-404 <katya-afk@mail.ru>
2022-12-07 13:52:45 +0700

    Юху

--- web/app/src/main/java/com/example/webex/login.java
+++ web/app/src/main/java/com/example/webex/login.java
@@ -1,5 +1,7 @@
 package com.example.webex;
 
+import static com.example.webex.md5.encrypt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,10 +9,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class login extends AppCompatActivity {
     private DBHelper dbHelper  = new DBHelper();
@@ -28,23 +32,19 @@ public class login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    dbHelper.authorize(text_login.getText().toString(),
-                            text_password.getText().toString(),
+                    String pass = text_password.getText().toString();
+                    pass = encrypt(pass);
+                    dbHelper.authorize(
+                            text_login.getText().toString(),
+                            pass,
                             ()->{openPersonalAccount(); return null;},
                             ()->{loginFailed(); return null;}
                             );
                 } catch (UnsupportedEncodingException e){
                     e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-            }
-        });
-
-        Button web = (Button) findViewById(R.id.web);
-        web.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(login.this, browser.class);
-                startActivity(intent);
             }
         });
     }
